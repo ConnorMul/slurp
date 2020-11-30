4b2ce0fffe10e56e7e8d8c41d751dadb0341ba59
ConnorMul <mulhollandconnor@icloud.com>
2020-11-30 15:36:49 -0500

    added models

---
+++ db/schema.rb
@@ -0,0 +1,65 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_11_30_203126) do
+
+  create_table "add_cocktail_to_kitchens", force: :cascade do |t|
+    t.integer "cocktail_id", null: false
+    t.integer "kitchen_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_add_cocktail_to_kitchens_on_cocktail_id"
+    t.index ["kitchen_id"], name: "index_add_cocktail_to_kitchens_on_kitchen_id"
+  end
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "drink"
+    t.string "glass"
+    t.text "instructions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "kitchens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_kitchens_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "stars"
+    t.integer "user_id", null: false
+    t.integer "cocktail_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "password_confirmation"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "add_cocktail_to_kitchens", "cocktails"
+  add_foreign_key "add_cocktail_to_kitchens", "kitchens"
+  add_foreign_key "kitchens", "users"
+  add_foreign_key "reviews", "cocktails"
+  add_foreign_key "reviews", "users"
+end
